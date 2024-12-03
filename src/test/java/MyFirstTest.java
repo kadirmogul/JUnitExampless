00894e134557a29decfdc67218ae51715dde203f
kadirmogul <158027198+kadirmogul@users.noreply.github.com>
2024-12-03 16:56:18 +0300

    Update MyFirstTest.java

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,3 +1,41 @@
-public class MyTest {
-    
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.DriverManager;
+import java.time.Duration;
+
+public class MyFirstTest {
+
+  static  WebDriver driver;
+
+@BeforeClass
+  public static void testPreparation(){
+      WebDriverManager.chromedriver().setup();
+      driver=new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+  }
+    @Test
+    public void testCase1() {
+    driver.get("https://www.google.com.tr/");
+    String actualData=driver.getTitle();
+    String expectedData="gOOgle";
+   // Assert.assertTrue(actualData.contains(expectedData));
+    Assert.assertEquals(actualData.toLowerCase(),expectedData.toLowerCase());
+
+
+  }
+
+@AfterClass
+    public static void endTest(){
+    driver.quit();
 }
+
+
+}
+
+
+
